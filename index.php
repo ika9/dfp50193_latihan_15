7e45817a1f67b480e7e1dfc6de26d0351e302886
ika9 <nuraziqah92@gmail.com>
2021-10-25 12:58:30 +0800

    index
    index dan proses

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pelbagai Soalan</title>
+</head>
+
+<body>
+    <form action="proses.php" method="post">
+        Nama
+        <input type="text" name="nama">
+        <br>
+        Password
+        <input type="password" name="kata">
+        <br>
+        <button type="submit">GO</button>
+
+    </form>
+
+</body>
+
+</html>
